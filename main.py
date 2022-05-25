679fb7f8c6d3f931a742ec5169a505c183b24101
aseemgupta02 <80508397+aseemgupta02@users.noreply.github.com>
2022-05-25 15:18:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def hello_world(request):
     elif request_json and 'message' in request_json:
         return request_json['message']
     else:
-        return f'Hello cloud advocates, how are you!!!'
+        return f'Hello cloud advocates hey!!!'
